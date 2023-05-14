48b4c24e5bd5373907f01c340cb17b735aafba88
wiel <yahiesdev@gmail.com>
2023-05-13 22:12:59 -0500

    prod

--- src/Fetchs/Base_URL.js
+++ src/Fetchs/Base_URL.js
@@ -1,4 +1,4 @@
 //export const BaseURLAPI="https://gohcomputerserver.onrender.com/componentes;
 export const BaseURLAPI="http://localhost:4004";
 
-export const BaseURLAPI2="http://localhost:4005";
+export const BaseURLAPI2="https://goh-computer.onrender.com";
